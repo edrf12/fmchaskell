39466a89500448860b3ab5a8625b17b038416d56
edrf12 <me@edrf.party>
2025-09-29 18:25:07 -0300

    Finish FMCBool.hs

--- Solutions/FMCBool.hs
+++ Solutions/FMCBool.hs
@@ -26,67 +26,82 @@ data Bool = False | True
 
 instance Show Bool where
 
-    show = undefined
+  show False = "False"
+  show True = "True"
 
 instance Enum Bool where
 
-    toEnum  = undefined
+    toEnum 0 = False
+    toEnum _ = True
 
-    fromEnum  = undefined
+    fromEnum False = 0
+    fromEnum True = 1
 
 -- conjunction (AND)
 (&&) :: Bool -> Bool -> Bool
-(&&) = undefined
+True && True = True
+_ && _ = False
 
 infixr 3 &&
 
 -- disjunction (OR)
 (||) :: Bool -> Bool -> Bool
-(||) = undefined
+False || False = False
+_ || _ = True
 
 infixr 2 ||
 
 -- NAND (Sheffer stroke)
 (/|\) :: Bool -> Bool -> Bool
-(/|\) = undefined
+True /|\ True = False
+_ /|\ _ = True
 
 infixr 2 /|\
 
 -- NOR (aka: Peirce arrow or Quine dagger)
 (\|/) :: Bool -> Bool -> Bool
-(\|/) = undefined
+False \|/ False = True
+_ \|/ _ = False
 
 infixr 2 \|/
 
 -- XOR (exclusive disjunction)
 (<=/=>) :: Bool -> Bool -> Bool
-(<=/=>) = undefined
+True <=/=> False = True
+False <=/=> True = True
+_ <=/=> _ = False
+
 
 infixr 2 <=/=>
 
 -- boolean negation
 not :: Bool -> Bool
-not = undefined
+not True = False
+not False = True
 
 -- if-then-else expression
 ifThenElse :: Bool -> a -> a -> a
-ifThenElse = undefined
+ifThenElse True a _ = a
+ifThenElse False _ a = a
 
 -- logical "implies"
 (==>) :: Bool -> Bool -> Bool
-(==>) = undefined
+True ==> False = False
+_ ==> _ = True
 
 infixr 1 ==>
 
 -- logical "implied by"
 (<==) :: Bool -> Bool -> Bool
-(<==) = undefined
+p <== p' = p' ==> p
 
 infixl 1 <==
 
 -- logical equivalence
 (<=>) :: Bool -> Bool -> Bool
-(<=>) = undefined
+True <=> True = True
+False <=> False = True
+_ <=> _ = False
 
 infixr 1 <=>
 
